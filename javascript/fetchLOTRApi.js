74dd18d76800f44a91f46f6f3843ef7d853f9982
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-22 15:19:07 +0100

    api call working

--- javascript/fetchLOTRApi.js
+++ javascript/fetchLOTRApi.js
@@ -3,10 +3,13 @@ import { LOTR_API_KEY } from '../config/dev.js';
 export async function fetchLOTRApi(characterId) {
   let requestOptions = {
     headers: {
-      Bearer: LOTR_API_KEY,
+      Authorization: `Bearer ${LOTR_API_KEY}`,
     },
   };
   let response = await fetch(
-    `https://the-one-api.dev/v2/character/${characterId}`
+    `https://the-one-api.dev/v2/quote`,
+    requestOptions
   );
+  let quotes = await response.json();
+  console.log(quotes);
 }
--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ import { StartGame } from './startGame.js';
 import { getUserName } from './getUserName.js';
 import { LOTR_API_KEY } from '../config/dev.js';
 import { storeFavUniverse } from './storeFavUniverse.js';
+import { fetchLOTRApi } from './fetchLOTRApi.js';
 
 // Explain let vs const for object
 export const mainData = {
@@ -11,7 +12,9 @@ export const mainData = {
   favUniverse: '',
   questionQuote: '',
   correctAnswer: '',
-  incorrectAnswers: [],
+  incorrectAnswers: ['1', '2', 345679013],
   bonusTimerCounter: 0,
   roundsPlayedCounter: 0,
 };
+
+fetchLOTRApi('');
