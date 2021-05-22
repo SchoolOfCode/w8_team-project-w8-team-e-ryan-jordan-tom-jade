40ecbf1deb97e2e9aa7578bf8cbb9cb3ecb332e9
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-22 12:05:02 +0100

    api keys and gitignore

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,16 @@
+import { StartGame } from './startGame.js';
+import { getUserName } from './getUserName.js';
+import { LOTR_API_KEY } from './config/dev.js';
 
-import {StartGame} from "./startGame.js"
-import { getUserName } from "./getUserName.js"
-
-
+// Explain let vs const for object
 const mainData = {
-    score: 0,
-    userInput: getUserName,
-    ApiResponse: "",
-    favUniverse: "",
-    questionQuote: "",
-    correctAnswer: "",
-    incorrectAnswers: [],
-    bonusTimerCounter: 0,
-    roundsPlayedCounter: 0
-
+  score: 0,
+  userInput: getUserName,
+  ApiResponse: '',
+  favUniverse: '',
+  questionQuote: '',
+  correctAnswer: '',
+  incorrectAnswers: [],
+  bonusTimerCounter: 0,
+  roundsPlayedCounter: 0,
 };
-
-
