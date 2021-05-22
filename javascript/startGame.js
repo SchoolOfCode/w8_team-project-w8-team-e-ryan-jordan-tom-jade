f5fbc54bd3e63ca1aeb1f1e652571ea20d8fa4c1
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-05-22 14:34:01 +0100

    added no function to wipe selected and hide area

--- javascript/startGame.js
+++ javascript/startGame.js
@@ -1,7 +1,7 @@
 // make page start game area once universe is clicked
-import mainData
+import {mainData} from "./main.js"
 export function StartGame(){
-  changeHeight()
+
   let favUnipick = mainData.favUniverse
   let startbutton = document.querySelector(".page__main__startgame--start")
   startbutton.innerText = favUnipick
@@ -9,8 +9,13 @@ export function StartGame(){
 
   // 2. This is to show the button and change the high of the button area
 
-  function changeHeight(){
-    let getStartGameArea = document.querySelector(".page__main__startgame")
-    getStartGameArea.style.height = "20%"
-  }
+  let getStartGameArea = document.querySelector(".page__main__startgame")
+  getStartGameArea.style.height = "20%"
+  let displayUniverse = document.querySelector(".page__main__startgame--start")
+  let displayNo = document.querySelector(".page__main__startgame--yes")
+  let displayYes = document.querySelector(".page__main__startgame--no")
+  displayNo.style.display = "block"
+  displayUniverse.style.display = "block"
+  displayYes.style.display = "block"
+  
 }
