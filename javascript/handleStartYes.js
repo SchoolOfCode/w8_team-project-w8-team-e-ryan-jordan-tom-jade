b83e51e08fa2717511f419ecbd965e74a4f10c6e
Jade Phipps <jadelph@outlook.com>
2021-05-22 16:36:00 +0100

    changes made

--- javascript/handleStartYes.js
+++ javascript/handleStartYes.js
@@ -1,13 +1,12 @@
 import { mainData } from "./main.js"
 import { buildQuestionLayout } from "./buildQuestionLayout"
 
+document.querySelector(".page__main__startgame--yes").addEventListener("click", handleStartYes)
+
+
 export function handleStartYes() {
     //listen for yes or no click from start screen
     // if click = yes then toggle to gameplay screen 
     // turn off the start screen 
-    document.querySelector(".page__main__startgame--yes").addEventListener("click", buildQuestionLayout())
-
-
-
-
+    //select the screen container ("page") off for the start screen call the build question ()
 }
