98a076242767cb551a24dbfeeae2f75e14043436
Jade Phipps <jadelph@outlook.com>
2021-05-22 17:08:28 +0100

    handle start yes turns off start screen

--- javascript/handleStartYes.js
+++ javascript/handleStartYes.js
@@ -8,5 +8,9 @@ export function handleStartYes() {
     //listen for yes or no click from start screen
     // if click = yes then toggle to gameplay screen 
     // turn off the start screen 
+    let turnOffStartPage = document.querySelector(".page")
+    turnOffStartPage.style.display = 'none';
+    buildQuestionLayout();
+
     //select the screen container ("page") off for the start screen call the build question ()
 }
