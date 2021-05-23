f429eaf318364fb0417ed61e975885d4a5dad2ad
Jade Phipps <jadelph@outlook.com>
2021-05-23 18:12:04 +0100

    handle yes complete

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,6 @@
 import { StartGame } from './startGame.js';
 import { handleStartNo } from './handleStartNo.js';
+import { handleStartYes } from './handleStartYes.js';
 import { getUserName } from './getUserName.js';
 import { LOTR_API_KEY } from '../config/dev.js';
 import { storeFavUniverse } from './storeFavUniverse.js';
@@ -9,20 +10,18 @@ import { buildMultipleAnswers } from './buildMultipleAnswers.js';
 import { startTimer } from './startTimer.js';
 import { fetchPokemonApi } from './fetchPokemonApi.js';
 
+
 // Explain let vs const for object
 export const mainData = {
-  score: 0,
-  userInput: getUserName,
-  ApiResponse: '',
-  favUniverse: '',
-  questionQuote: '',
-  correctAnswer: 'Gollum',
-  incorrectAnswers: [],
-  bonusTimerCounter: 20,
-  roundsPlayedCounter: 0,
-  firstQuestion: false,
-  activeUniverse: 'LOTR',
+    score: 0,
+    userInput: getUserName,
+    ApiResponse: '',
+    favUniverse: '',
+    questionQuote: '',
+    correctAnswer: 'Gollum',
+    incorrectAnswers: [],
+    bonusTimerCounter: 20,
+    roundsPlayedCounter: 0,
+    firstQuestion: false,
+    activeUniverse: 'LOTR',
 };
-
-buildQuestionLayout();
-
