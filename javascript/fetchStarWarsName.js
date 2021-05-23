5094fe0ade1ce5da38adbb84bc7d33f64f0c7cc0
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-05-24 00:33:30 +0100

    made a checker to keep names with only first and secound names

--- javascript/fetchStarWarsName.js
+++ javascript/fetchStarWarsName.js
@@ -1,28 +1,40 @@
 import {mainData} from "./main.js"
 
-
-
 export async function getStarWarsNames(){
 
   let randomPage = Math.floor(Math.random()*6)
-
-  
   
   async function fetchNames(){
     let pageResults = `http://swapi.dev/api/people/?page=${randomPage+1}`
     const response = await fetch(pageResults)
     let data = await response.json()
-    console.log(data.results) 
     return data.results
   }
-
-  let list1 = fetchNames()
+  let list1 = await fetchNames()
   randomPage++
-  let list2 = fetchNames()
-  
-  return mainData.listOfStarWarsNames = [await list1, await list2]
-  
+  let list2 = await fetchNames()
 
+  let list3 = list1.concat(list2)
+  let checker = 0
+  let extractNames = []
+  list3.forEach((item)=>{
+    for(let i =0;i<item.name.length;i++){
+      if(item.name[i]==" "){
+        
+        checker++
+      }
+    }
+    if(checker==1){
+      extractNames.push( item.name)
+    }
+    checker=0
+    
+    //item.name.indexOf(" ") >= 1 && item.name.indexOf(" ") >= 1
+
+  })
+
+  return mainData.listOfStarWarsNames = await extractNames
+  
 }
 
 
