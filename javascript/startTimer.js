f5e9650b16585df3e1127f360d06b6ee0cde0e39
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-25 16:05:14 +0100

    timer bug fixed except star wars time

--- javascript/startTimer.js
+++ javascript/startTimer.js
@@ -1,27 +1,29 @@
 import { buildQuestionLayout } from './buildQuestionLayout.js';
 import { mainData } from './main.js';
-import { gameOver} from "./gameOver.js"
+import { gameOver } from './gameOver.js';
 let timer = 20;
 let getTimerBox = document.querySelector('.page2__main2__tophalf__timer');
 
 export function startTimer(time) {
-  if(mainData.activeUniverse == 'StarWars'){
-    mainData.bonusTimerCounter = 40
+  if (mainData.activeUniverse == 'StarWars') {
+    mainData.bonusTimerCounter = 40;
   }
-
 
   let countdown = setInterval(() => {
     if (mainData.bonusTimerCounter <= 0) {
-      if(mainData.activeUniverse == 'LOTR'){
+      if (
+        mainData.activeUniverse === 'LOTR' ||
+        mainData.activeUniverse === 'pokemon'
+      ) {
         buildQuestionLayout();
       }
-      if(mainData.activeUniverse == 'StarWars'){
+      if (mainData.activeUniverse === 'StarWars') {
         clearInterval(countdown);
-        gameOver()
+        gameOver();
       }
-      clearInterval(countdown);
-      mainData.bonusTimerCounter = 20;
-      return;
+      // clearInterval(countdown);
+      // mainData.bonusTimerCounter = 20;
+      // return;
     }
     getTimerBox.innerText = mainData.bonusTimerCounter;
     mainData.bonusTimerCounter--;
