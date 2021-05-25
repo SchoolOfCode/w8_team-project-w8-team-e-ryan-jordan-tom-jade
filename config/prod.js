a588e77e5b55db5125e84cdae16b7d33e483804b
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-25 19:48:48 +0100

    updated process variable

---
+++ config/prod.js
@@ -0,0 +1,1 @@
+export const LOTR_API_KEY = process.env.LOTR_API_KEY;
--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,6 @@ import { StartGame } from './startGame.js';
 import { handleStartNo } from './handleStartNo.js';
 import { handleStartYes } from './handleStartYes.js';
 import { getUserName } from './getUserName.js';
-import { LOTR_API_KEY } from '../config/dev.js';
 import { storeFavUniverse } from './storeFavUniverse.js';
 import { fetchLOTRApi } from './fetchLOTRApi.js';
 import { buildQuestionLayout } from './buildQuestionLayout.js';
@@ -11,12 +10,12 @@ import { startTimer } from './startTimer.js';
 import { fetchPokemonApi } from './fetchPokemonApi.js';
 import { gameOver } from './gameOver.js';
 import { getStarWarsNames } from './fetchStarWarsName.js';
-import { NameMatchChecker} from "./buildNameMatchChecker.js"
+import { NameMatchChecker } from './buildNameMatchChecker.js';
 
 // Explain let vs const for object
 export const mainData = {
   score: 0,
-  userInput:"",
+  userInput: '',
   ApiResponse: '',
   favUniverse: '',
   questionQuote: '',
@@ -31,7 +30,5 @@ export const mainData = {
   },
   firstQuestion: false,
   activeUniverse: '',
-  listOfStarWarsNames:""
-
+  listOfStarWarsNames: '',
 };
-
