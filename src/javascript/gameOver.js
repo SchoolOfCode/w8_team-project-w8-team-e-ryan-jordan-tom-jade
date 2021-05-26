65c28bffac949259710acc0a98c874a8791eb9ba
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-26 17:25:40 +0100

    bugs fixed

--- src/javascript/gameOver.js
+++ src/javascript/gameOver.js
@@ -1,32 +1,30 @@
-import { mainData } from "./main.js"
-import { handleStartNo } from "./handleStartNo.js"
+import { mainData } from './main.js';
+import { handleStartNo } from './handleStartNo.js';
 
-let displayPage = document.querySelector(".gameOver")
-let backToMenu = document.querySelector(".gameOver__main__content--menu")
-backToMenu.addEventListener("click", goBackToMenu)
+let displayPage = document.querySelector('.gameOver');
+let backToMenu = document.querySelector('.gameOver__main__content--menu');
+backToMenu.addEventListener('click', goBackToMenu);
 
 export function gameOver() {
+  let unDisplayPage2 = document.querySelector('.page2');
+  let getUserArea = document.querySelector('.gameOver__main__content--user');
+  getUserArea.innerText = mainData.userInput;
+  displayPage.style.display = 'flex';
+  unDisplayPage2.style.display = 'none';
 
-  let unDisplayPage2 = document.querySelector(".page2")
-  let getUserArea = document.querySelector(".gameOver__main__content--user")
-  getUserArea.innerText = mainData.userInput
-  displayPage.style.display = "flex"
-  unDisplayPage2.style.display = "none"
+  let getPoints = document.querySelector('.gameOver__main__content--points');
+  getPoints.innerText = `${mainData.score.total} Points`;
 
-
-  let getPoints = document.querySelector(".gameOver__main__content--points")
-  getPoints.innerText = `${mainData.score} Points`
   console.log('game over called');
-  
 }
 
-export function goBackToMenu(){
-  let turnOnPage1 = document.querySelector(".page")
-    turnOnPage1.style.display = 'flex';
-  displayPage.style.display = "none"
-  mainData.score = 0
-  mainData.roundsCompleted.LOTR = false
-  mainData.roundsCompleted.pokemon = false
-  mainData.roundsCompleted.starWars = false
-  handleStartNo()
+export function goBackToMenu() {
+  let turnOnPage1 = document.querySelector('.page');
+  turnOnPage1.style.display = 'flex';
+  displayPage.style.display = 'none';
+  mainData.score = 0;
+  mainData.roundsCompleted.LOTR = false;
+  mainData.roundsCompleted.pokemon = false;
+  mainData.roundsCompleted.starWars = false;
+  handleStartNo();
 }
--- src/javascript/setPoints.js
+++ src/javascript/setPoints.js
@@ -1,9 +1,26 @@
 import { mainData } from './main.js';
 
 export function setPoints() {
-  mainData.score += 10;
-
-  if (mainData.bonusTimerCounter >= 15) {
-    mainData.score += 5;
+  if (mainData.activeUniverse === 'LOTR') {
+    mainData.score.LOTR += 10;
+    if (mainData.bonusTimerCounter >= 15) {
+      mainData.score.LOTR += 5;
+    }
   }
+
+  if (mainData.activeUniverse === 'pokemon') {
+    mainData.score.pokemon += 10;
+    if (mainData.bonusTimerCounter >= 15) {
+      mainData.score.pokemon += 5;
+    }
+  }
+
+  if (mainData.activeUniverse === 'StarWars') {
+    mainData.score.starWars += 10;
+    if (mainData.bonusTimerCounter >= 15) {
+      mainData.score.starWars += 5;
+    }
+  }
+  mainData.score.total =
+    mainData.score.LOTR + mainData.score.pokemon + mainData.score.starWars;
 }
