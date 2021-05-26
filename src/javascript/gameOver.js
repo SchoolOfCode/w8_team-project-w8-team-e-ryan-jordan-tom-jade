f0759d110271228d59a7af1347ffb7cdf889aa8e
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-26 17:38:15 +0100

    score breakdown and rounds counter

--- src/javascript/gameOver.js
+++ src/javascript/gameOver.js
@@ -1,5 +1,6 @@
 import { mainData } from './main.js';
 import { handleStartNo } from './handleStartNo.js';
+import { createNewElement } from './createNewElement.js';
 
 let displayPage = document.querySelector('.gameOver');
 let backToMenu = document.querySelector('.gameOver__main__content--menu');
@@ -14,6 +15,22 @@ export function gameOver() {
 
   let getPoints = document.querySelector('.gameOver__main__content--points');
   getPoints.innerText = `${mainData.score.total} Points`;
+  let LOTRscore = createNewElement(
+    'p',
+    `LOTR: ${mainData.score.LOTR} points`,
+    'sub-score'
+  );
+  let pokemonScore = createNewElement(
+    'p',
+    `Pokemon: ${mainData.score.pokemon} points`,
+    'sub-score'
+  );
+  let starWarsScore = createNewElement(
+    'p',
+    `Star Wars: ${mainData.score.starWars} points`,
+    'sub-score'
+  );
+  getPoints.append(LOTRscore, pokemonScore, starWarsScore);
 
   console.log('game over called');
 }
@@ -22,7 +39,10 @@ export function goBackToMenu() {
   let turnOnPage1 = document.querySelector('.page');
   turnOnPage1.style.display = 'flex';
   displayPage.style.display = 'none';
-  mainData.score = 0;
+  mainData.score.total = 0;
+  mainData.score.LOTR = 0;
+  mainData.score.pokemon = 0;
+  mainData.score.starWars = 0;
   mainData.roundsCompleted.LOTR = false;
   mainData.roundsCompleted.pokemon = false;
   mainData.roundsCompleted.starWars = false;
